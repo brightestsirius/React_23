88191c355e84006bc265bb4176556e22195ad7c2
brightestsirius <katherina.shypovskaya@gmail.com>
2023-09-19 16:25:23 +0300

    add data

--- final_project/src/components/CapitalForm/CapitalForm.jsx
+++ final_project/src/components/CapitalForm/CapitalForm.jsx
@@ -1,32 +1,38 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import "./style.sass";
 
 export default function CapitalForm() {
   const navigation = useNavigate();
 
   const countries = useSelector((store) => store.countries);
-  const [translations, setTranslations] = useState(Object.keys(countries[0].translations));
+  const [translations, setTranslations] = useState(
+    countries.length ? Object.keys(countries[0].translations) : []
+  );
 
-  const [country, setCountry] = useState(countries[0].name.official);
+  const [country, setCountry] = useState(
+    countries[0] ? countries[0].name.official : {}
+  );
   const [translation, setTranslation] = useState(translations[0]);
 
-  const handleSelectCapital = e => {
+  const handleSelectCapital = (e) => {
     setCountry(e.target.value);
-    let countryTranslations = countries.find(country => country.name.official === e.target.value).translations;
+    let countryTranslations = countries.find(
+      (country) => country.name.official === e.target.value
+    ).translations;
     setTranslations(Object.keys(countryTranslations));
     setTranslation(Object.keys(countryTranslations)[0]);
   };
 
-  const handleSelectTranslation = e => setTranslation(e.target.value);
+  const handleSelectTranslation = (e) => setTranslation(e.target.value);
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     navigation(`/countries/${country}?translation=${translation}`);
-  }
+  };
 
-  return (
+  return countries.length ? (
     <div className="component">
       <h3>Capital Form Component</h3>
       <form onSubmit={handleSubmit}>
@@ -44,12 +50,14 @@ export default function CapitalForm() {
           <h4>Select Translation</h4>
           <select defaultValue={translation} onChange={handleSelectTranslation}>
             {translations.map((key, index) => (
-              <option key={index} value={key}>{key}</option>
+              <option key={index} value={key}>
+                {key}
+              </option>
             ))}
           </select>
         </label>
         <button>Read more about {country}</button>
       </form>
     </div>
-  );
+  ) : null;
 }
---
+++ final_project/src/store/data.js
@@ -0,0 +1,149 @@
+const INITIAL_STATE = {
+  countries: [
+    {
+      name: {
+        official: "Ukraine",
+        common: "Україна",
+      },
+      currencies: {
+        UAH: {
+          name: "Ukrainian hryvnia",
+          symbol: "₴",
+        },
+      },
+      capital: ["Kyiv"],
+      region: "Europe",
+      subregion: "Eastern Europe",
+      languages: {
+        ukr: "Ukrainian",
+      },
+      flag: "🇺🇦",
+      translations: {
+        ara: {
+          official: "أوكرانيا",
+          common: "أوكرانيا",
+        },
+        bre: {
+          official: "Ukraina",
+          common: "Ukraina",
+        },
+        ces: {
+          official: "Ukrajina",
+          common: "Ukrajina",
+        },
+      },
+    },
+    {
+      name: {
+        official: "Kingdom of Norway",
+        common: "Kongeriket Noreg",
+      },
+      currencies: {
+        NOK: {
+          name: "Norwegian krone",
+          symbol: "kr",
+        },
+      },
+      capital: ["Oslo"],
+      region: "Europe",
+      subregion: "Northern Europe",
+      languages: {
+        nno: "Norwegian Nynorsk",
+        nob: "Norwegian Bokmål",
+        smi: "Sami",
+      },
+      flag: "🇳🇴",
+      translations: {
+        fra: {
+          official: "Royaume de Norvège",
+          common: "Norvège",
+        },
+        hrv: {
+          official: "Kraljevina Norveška",
+          common: "Norveška",
+        },
+        hun: {
+          official: "Norvég Királyság",
+          common: "Norvégia",
+        },
+      },
+    },
+    {
+      name: {
+        official: "Canada",
+        common: "Canada",
+      },
+      currencies: {
+        CAD: {
+          name: "Canadian dollar",
+          symbol: "$",
+        },
+      },
+      capital: ["Ottawa"],
+      region: "Americas",
+      subregion: "North America",
+      languages: {
+        eng: "English",
+        fra: "French",
+      },
+      flag: "🇨🇦",
+      translations: {
+        ita: {
+          official: "Canada",
+          common: "Canada",
+        },
+        jpn: {
+          official: "カナダ",
+          common: "カナダ",
+        },
+        kor: {
+          official: "캐나다",
+          common: "캐나다",
+        },
+        nld: {
+          official: "Canada",
+          common: "Canada",
+        },
+      },
+    },
+    {
+      name: {
+        official: "Independent State of Papua New Guinea",
+        common: "Papua Niu Gini",
+      },
+      currencies: {
+        PGK: {
+          name: "Papua New Guinean kina",
+          symbol: "K",
+        },
+      },
+      capital: ["Port Moresby"],
+      region: "Oceania",
+      subregion: "Melanesia",
+      languages: {
+        eng: "English",
+        hmo: "Hiri Motu",
+        tpi: "Tok Pisin",
+      },
+      flag: "🇵🇬",
+      translations: {
+        hun: {
+          official: "Pápua Új-Guinea Független Állam",
+          common: "Pápua Új-Guinea",
+        },
+        ita: {
+          official: "Stato indipendente di Papua Nuova Guinea",
+          common: "Papua Nuova Guinea",
+        },
+        por: {
+          official: "Estado Independente da Papua Nova Guiné",
+          common: "Papua Nova Guiné",
+        },
+        slk: {
+          official: "Nezávislý štát Papua-Nová Guinea",
+          common: "Papua-Nová Guinea",
+        },
+      },
+    },
+  ],
+};
