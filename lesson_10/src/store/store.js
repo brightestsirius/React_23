5001a7baf23e666462070bd038d35bf82c4a8ae6
brightestsirius <katherina.shypovskaya@gmail.com>
2023-09-21 18:46:39 +0300

    lesson_10 – redux && react-redux && redux-thunk && redux-toolkit

--- lesson_10/src/store/store.js
+++ lesson_10/src/store/store.js
@@ -1,12 +1,10 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import reduxThunk from "redux-thunk";
-import reduxLogger from "redux-logger";
+import { configureStore } from '@reduxjs/toolkit'
 
 import todo from "./todo/reducer";
 import filter from "./filter/reducer";
 
-const rootReducer = combineReducers({ todo, filter });
-
-const store = createStore(rootReducer, applyMiddleware(reduxLogger, reduxThunk));
+const store = configureStore({
+    reducer: {todo, filter},
+  })
 
 export default store;
